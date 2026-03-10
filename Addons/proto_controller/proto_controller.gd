f5655ef6b28bc52072706ff0ec4827414b9ad3a9
TheFefel <flxkhn0602@gmail.com>
2026-03-10 23:46:44 +0100

    fix: camera bug

--- Addons/proto_controller/proto_controller.gd
+++ Addons/proto_controller/proto_controller.gd
@@ -52,6 +52,7 @@ var freeflying : bool = false
 ## IMPORTANT REFERENCES
 @onready var head: Node3D = $Head
 @onready var collider: CollisionShape3D = $Collider
+@onready var camera_3d: Camera3D = $Head/Camera3D
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
@@ -60,6 +61,7 @@ func _ready() -> void:
 	check_input_mappings()
 	look_rotation.y = rotation.y
 	look_rotation.x = head.rotation.x
+	camera_3d.current = is_multiplayer_authority()
 
 func _unhandled_input(event: InputEvent) -> void:
 	# Mouse capturing
