ac7fe95c6c789519e152ddd5a2447d99898b0cf2
TheFefel <flxkhn0602@gmail.com>
2026-03-19 14:07:08 +0100

    fix: trying to fix rpc bug still

--- Scenes/Levels/game_room.gd
+++ Scenes/Levels/game_room.gd
@@ -35,8 +35,9 @@ func _add_player(id: int, index: int):
 	player.name = str(id)
 	player_spawn.add_child(player, true)
 	player.global_position = get_spawn_position(index, total_players)
+	player.set_look_at(Vector3.ZERO)
+	await get_tree().process_frame
 	player.rpc_id(id, "set_player_bools", false, false)
-	player.set_look_at(Vector3.UP)
 
 func _remove_player(id: int):
 	if not player_spawn.has_node(str(id)):
